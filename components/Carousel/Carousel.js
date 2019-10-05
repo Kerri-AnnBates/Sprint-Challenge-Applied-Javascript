74848c8c3948f4a9a889ae3729fa878b05847f08
Kerri-Ann <kaybee1jam@yahoo.com>
2019-10-05 17:26:28 -0400

    finished basic functionality of carousel

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -22,7 +22,6 @@ function carouselComponent() {
   const carouselDiv = document.createElement('div'),
   leftButton = document.createElement('div'),
   rightButton = document.createElement('div');
-  let image = document.createElement('img');
 
   
   // setup classes
@@ -35,20 +34,55 @@ function carouselComponent() {
   leftButton.textContent = '\u2039';
 
   // Setup structure
+  carouselDiv.appendChild(leftButton);
   const images = ['computer.jpeg', 'mountains.jpeg', 'trees.jpeg', 'turntable.jpeg'];
-  images.forEach(img => {
-    image = document.createElement('img');
+  images.forEach((img, i) => {
+    let image = document.createElement('img');
     image.src = `./assets/carousel/${img}`;
-    carouselDiv.appendChild(leftButton);
     carouselDiv.appendChild(image);
-    carouselDiv.appendChild(rightButton);
+    
   });
+  carouselDiv.appendChild(rightButton);
 
-  currentImg = 0;
-
-  
   return carouselDiv;
 }
 
 const carouselContainer = document.querySelector('.carousel-container');
 carouselContainer.appendChild(carouselComponent());
+
+// Set up functionality for carousel.
+const carouselImages = document.querySelectorAll('.carousel img'),
+nextBtn = document.querySelector('.right-button'),
+prevBtn = document.querySelector('.left-button');
+
+let currIndex = 0;
+
+// Set first image to display: block
+carouselImages[currIndex].style.display = 'block';
+
+// Set up next button functionality
+nextBtn.addEventListener('click', (e) => {
+  currIndex++;
+  // console.log(currIndex);
+  if (currIndex < carouselImages.length) {
+    carouselImages[currIndex].style.display = 'block';
+    carouselImages[currIndex - 1].style.display = 'none';
+  } else {
+    currIndex = 0;
+    carouselImages[currIndex].style.display = 'block';
+    carouselImages[carouselImages.length-1].style.display = 'none';
+  }
+});
+
+prevBtn.addEventListener('click', (e) => {
+  currIndex--;
+  // console.log(currIndex);
+  if (currIndex >= 0) {
+    carouselImages[currIndex].style.display = 'block';
+    carouselImages[currIndex + 1].style.display = 'none';
+  } else {
+    currIndex = carouselImages.length - 1;
+    carouselImages[currIndex].style.display = 'block';
+    carouselImages[0].style.display = 'none';
+  }
+});
